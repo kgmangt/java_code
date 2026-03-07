4133a2f36db513288c86de741d4fc7ad4fff7640
kgmangt <153043341+kgmangt@users.noreply.github.com>
2026-03-07 16:18:17 +0900

    Create 1330.java

---
+++ BAEKJOON/1330.java
@@ -0,0 +1,15 @@
+package practice;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        if(a>b) System.out.println(">");
+        else if(a<b) System.out.println("<");
+        else if(a==b) System.out.println("==");
+        sc.close(); 
+    }
+}
